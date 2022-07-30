6583a2979c82ca8c0c1cc0bba32c943caa3fc435
Mick Wiedermann <52492988+m1ckw@users.noreply.github.com>
2022-07-30 13:19:25 +1000

    Removed Unnecasary code and added comments.

--- Module01/m1_t1p_simple_sense_think_act_board1.ino
+++ Module01/m1_t1p_simple_sense_think_act_board1.ino
@@ -1,18 +1,19 @@
-
 const byte SENSOR_IN = 2;
-const byte LED_OUT = 12;
-const byte SM_OUT = 3;
+const byte LED_OUT = 3;
 
 void setup()
 {
+  // Initialise Serial Output
   Serial.begin(9600);
+  // Initialise Pin Functions
   pinMode(SENSOR_IN, INPUT);
   pinMode(LED_OUT, OUTPUT);
-  pinMode(SM_OUT, OUTPUT);
 }
 
+// Procedure for controlling the LED
 void ledAction() 
 {
+  // Just for fun to make the LED blink.
   for (int i=0; i<4; i++) 
   {
     digitalWrite(LED_OUT,HIGH);
@@ -24,20 +25,20 @@ void ledAction()
   digitalWrite(LED_OUT, LOW);
 }
 
+// Procedure for motion Detector.
 void motionDetected()
 {
   Serial.println("SENSOR: detected motion!"); 
   ledAction();
 }
 
+// Using Polling to update.
 void loop()
 {
-  digitalWrite(SM_OUT, LOW);
+  // Reads the state of the sensor.
   int read = digitalRead(SENSOR_IN);
   if(read == HIGH)
   {
     motionDetected();
   }
 }
-
-  
